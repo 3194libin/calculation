83858b9bde0684627bae23a408fb58e764c74379
3194libin <15764747737@qq.com>
2019-06-22 14:17:09 +0800

    Initial commit

---
+++ calculation.py
@@ -0,0 +1,9 @@
+class New_int(int):
+    def __add__(self, other):
+        return int.__sub__(self,other)
+    def __sub__(self, other):
+        return int.__add__(self,other)
+a=New_int(5)
+b=New_int(6)
+print(a+b)
+print(a-b)
